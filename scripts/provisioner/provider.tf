349891efc24238320037ab28d39c75afca329453
deviprasad <63544999+flipkartproject-dev@users.noreply.github.com>
2023-03-09 14:21:23 +0530

    Update provider.tf

--- scripts/provisioner/provider.tf
+++ scripts/provisioner/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
     region = "ap-south-1"
-    access_key = "AKIATHF6Z5VDQXSVLM6S"
-    secret_key = "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"
+    access_key = ""
+    secret_key = ""
 }
